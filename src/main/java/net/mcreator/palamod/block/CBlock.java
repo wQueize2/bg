f090fd7fdc621d9d60a34975a698740fbb7bfbe7
wQueize2 <piwiq@wQueize>
2022-10-11 19:18:37 +0200

    tr

--- src/main/java/net/mcreator/palamod/block/CBlock.java
+++ src/main/java/net/mcreator/palamod/block/CBlock.java
@@ -1,29 +1,58 @@
 
 package net.mcreator.palamod.block;
 
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
-import net.minecraft.world.level.material.Material;
+import net.minecraftforge.registries.ObjectHolder;
 
-public class CBlock extends Block {
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
 
-	public CBlock() {
-		super(BlockBehaviour.Properties.of(Material.ROCK).sound(SoundType.GROUND).strength(1f, 10f));
+import net.mcreator.palamod.PalaModModElements;
 
+import java.util.List;
+import java.util.Collections;
+
+@PalaModModElements.ModElement.Tag
+public class CBlock extends PalaModModElements.ModElement {
+	@ObjectHolder("pala_mod:c")
+	public static final Block block = null;
+
+	public CBlock(PalaModModElements instance) {
+		super(instance, 3);
 	}
 
 	@Override
-	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
-		return 15;
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
 	}
 
-	@Override
-	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
+			setRegistryName("c");
+		}
 
-		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
-		if (!dropsOriginal.isEmpty())
-			return dropsOriginal;
-		return Collections.singletonList(new ItemStack(this, 1));
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
 	}
-
 }
